5cbc955689b1349bfc87f61b23a28e4ff677ff6d
Benjamin Tessmar <b.tessmar@gmx.de>
2012-08-11 22:35:23 +0200

    Parameter der Anwendung definiert

--- src/ParallelPattern/Program.cs
+++ src/ParallelPattern/Program.cs
@@ -9,6 +9,32 @@ namespace ParallelPattern
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Beispiel für parallele Programmierung in c#.");
+            Console.WriteLine();
+
+            Console.WriteLine("Auswahlmöglichkeiten:");
+            Console.WriteLine("    [1] Sequentiel");
+            Console.WriteLine("    [2] Parallel");
+            Console.WriteLine();
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            string key = keyInfo.KeyChar.ToString();
+
+            switch (key)
+            {
+                case "1":
+
+                    break;
+                case "2":
+
+                    break;
+                default:
+                    Console.WriteLine("'" + key + "' steht nicht zur Auswahl.");
+                    break;
+            }
+
+            Console.Read();
         }
     }
 }
